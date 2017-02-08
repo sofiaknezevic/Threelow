399f67de4ec19955c04d42401435d56189889bb0
sofiaknezevic <sofia.knezevic@gmail.com>
2017-02-08 15:56:39 -0800

    Initial commit

---
+++ Threelow/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Threelow
+//
+//  Created by Sofia Knezevic on 2017-02-08.
+//  Copyright © 2017 Sofia Knezevic. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
